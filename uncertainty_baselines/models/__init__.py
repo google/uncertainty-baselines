e33984af057d4c4c6283731c2d7429a99a3ada73
Yongwhan Lim <yongwhan@google.com>
2020-12-11 12:17:23 -0800

    Add pretrained imagenet model to improve the performance.
    PiperOrigin-RevId: 347050247

--- uncertainty_baselines/models/__init__.py
+++ uncertainty_baselines/models/__init__.py
@@ -30,6 +30,7 @@ from uncertainty_baselines.models.resnet50_deterministic import resnet50_determi
 from uncertainty_baselines.models.resnet50_dropout import resnet50_dropout
 from uncertainty_baselines.models.resnet50_rank1 import resnet50_rank1
 from uncertainty_baselines.models.resnet50_sngp import resnet50_sngp
+from uncertainty_baselines.models.resnet50_sngp import resnet50_sngp_add_last_layer
 from uncertainty_baselines.models.resnet50_sngp_be import resnet50_sngp_be
 from uncertainty_baselines.models.textcnn import create_model as TextCNNBuilder
 from uncertainty_baselines.models.wide_resnet import create_model as WideResNetBuilder

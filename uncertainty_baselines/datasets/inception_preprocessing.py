445e6f1c5d96bd19b0eead7f9aa9bf3d1ad2c853
Zachary Nado <znado@google.com>
2021-02-21 14:56:49 -0800

    replacing custom `baselines.imagenet.utils.SyncBatchNorm` implementation with `tf.keras.layers.experimental.SyncBatchNormalization`, moving `baselines.imagenet.utils.MovingAverage` to `ub.optimizers.MovingAverage`.
    PiperOrigin-RevId: 358711263

--- uncertainty_baselines/datasets/inception_preprocessing.py
+++ uncertainty_baselines/datasets/inception_preprocessing.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
-
 """Provides utilities to preprocess images for the Inception networks."""
 
 from typing import Callable, Tuple, Optional

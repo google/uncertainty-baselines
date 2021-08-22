94e7d086827e102b53d7fd420c2268514f91c6b2
Qixuan Feng <ez7787fqx@gmail.com>
2021-08-22 02:50:56 +0200

    add ResNet50FSVI to models

--- uncertainty_baselines/models/__init__.py
+++ uncertainty_baselines/models/__init__.py
@@ -61,7 +61,7 @@ from uncertainty_baselines.models.wide_resnet_rank1 import wide_resnet_rank1
 from uncertainty_baselines.models.wide_resnet_sngp import wide_resnet_sngp
 from uncertainty_baselines.models.wide_resnet_sngp_be import wide_resnet_sngp_be
 from uncertainty_baselines.models.wide_resnet_variational import wide_resnet_variational
-
+from uncertainty_baselines.models.resnet50_fsvi import ResNet50FSVI
 # When adding a new model, also add to models.py for easier user access.
 
 # pylint: disable=g-import-not-at-top

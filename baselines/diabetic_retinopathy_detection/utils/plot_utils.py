cabd62bf71d2710a6c58b9622fcd978f49fc292f
Qixuan Feng <ez7787fqx@gmail.com>
2021-08-25 20:36:32 +0200

    finish implementation of plots

--- baselines/diabetic_retinopathy_detection/utils/plot_utils.py
+++ baselines/diabetic_retinopathy_detection/utils/plot_utils.py
@@ -1,13 +1,16 @@
 import os
 import pdb
 import time
-from typing import Dict, List, Tuple, NamedTuple, Callable, Union
+from collections import defaultdict
+from functools import partial
+from typing import Dict, List, Tuple, NamedTuple, Callable, Union, Any
 
 import numpy as np
 import pandas as pd
 import robustness_metrics as rm
 import tensorflow as tf
 import torch
+import tree
 from absl import flags
 from absl import logging
 from sklearn.metrics import (log_loss, roc_auc_score, accuracy_score,
@@ -23,11 +26,9 @@ import os.path as osp
 
 N_SAMPLES_PER_CLASS = {
   "aptos": [1805, 370, 999, 193, 295],
-  "eyepacs": {
-    "train": [25810, 2443, 5292, 873, 708],
-    "validation": [8130, 720, 1579, 237, 240],
-    "test": [31403, 3042, 6282, 977, 966],
-  }
+  "eyepacs_train": [25810, 2443, 5292, 873, 708],
+  "eyepacs_validation": [8130, 720, 1579, 237, 240],
+  "eyepacs_test": [31403, 3042, 6282, 977, 966],
 }
 
 
@@ -79,10 +80,25 @@ def plot_roc_curve(fpr, tpr, roc_auc, title='', plt_path='.'):
   plt.savefig(osp.join(plt_path, 'roc_curve.svg'))
 
 
-def plot_ece(y_true, y_pred, name="", n_windows=100, ax=None, color='blue'):
-  """
-  All inputs are 1D array
-  """
+def plot_pie_chart(x: Dict[str, float], ax=None, title=""):
+  if not ax:
+    plt.figure()
+    ax = plt.gca()
+  labels = sorted(x.keys())
+  vals = [x[l] for l in labels]
+  ax.pie(vals, labels=None, normalize=True, shadow=False, autopct='%1.1f%%',
+          startangle=-90, pctdistance=0.7, labeldistance=1.1)
+  ax.set_title(title)
+  plt.legend(labels=labels)
+  plt.tight_layout()
+
+
+def plot_pie_charts_all_dataset():
+  for name, array in N_SAMPLES_PER_CLASS.items():
+    plot_pie_chart({i: v for i, v in enumerate(array)}, title=name)
+
+
+def compute_data_for_ece_plot(y_true: np.ndarray, y_pred: np.ndarray, n_windows=100) -> Dict[str, np.ndarray]:
   probs = binary_converter(y_pred).flatten()
   labels = one_hot_encode(y_true, 2).flatten()
 
@@ -95,18 +111,25 @@ def plot_ece(y_true, y_pred, name="", n_windows=100, ax=None, color='blue'):
   # distances = []
   for i in range(len(probs_labels) - window_len):
     # distances.append((probs_labels[i + window_len, 0] - probs_labels[i, 0]) / float(window_len))
-    mean_confidences = mean(probs_labels[i:i + window_len, 0])
+    mean_confidences = mean_default_zero(probs_labels[i:i + window_len, 0])
     confidences.append(mean_confidences)
-    class_accuracies = mean(probs_labels[i:i + window_len, 1])
+    class_accuracies = mean_default_zero(probs_labels[i:i + window_len, 1])
     accuracies.append(class_accuracies)
+  return {
+    "accuracies": np.array(accuracies),
+    "confidences": np.array(confidences),
+  }
 
+
+def plot_ece(confidences, accuracies, label="", ax=None, **kwargs):
+  """
+  All inputs are 1D arrays
+  """
   if not ax:
     plt.figure()
     ax = plt.gca()
-
   # ax.figure.set_size_inches(6, 4)
-  ax.plot(confidences, accuracies, color=color, label=name)
-
+  ax.plot(confidences, accuracies, label=label, **kwargs)
   xbins = [i / 10. for i in range(11)]
   ax.plot(xbins, xbins, linestyle=':', color='black')
   ax.set_xlabel('Model Confidence')
@@ -127,7 +150,9 @@ class ModelKey(NamedTuple):
 
 
 RESULT_DICT = Dict[str, List[np.ndarray]]
-MODEL_RESULT_DICT = Dict[str, RESULT_DICT]
+# RESULT_DICT contains keys [‘y_pred’, ‘y_true’, ‘y_aleatoric_uncert’,
+#   ‘y_epistemic_uncert’, ‘y_total_uncert’, ‘is_ood']
+MODEL_RESULT_DICT = Dict[Tuple, RESULT_DICT]
 DATA_RESULT_DICT = Dict[str, MODEL_RESULT_DICT]
 
 
@@ -168,20 +193,33 @@ def plot_ece_all_methods(models_results_dict: MODEL_RESULT_DICT):
     fn=plot_ece_one_method,
     n_plots=len(models_results_dict),
     ncols=3,
-    get_args=[[k, v] for k, v in models_results_dict.items()],
+    get_args=[[parse_model_key(k), v] for k, v in models_results_dict.items()],
   )
 
 
-def plot_ece_one_method(model_key, result_dict, ax=None):
+def aggregate_result_dict(result_dict: RESULT_DICT, agg_fns: List[Callable], keys: List=None):
+  if keys:
+    result_dict = {k: v for k, v in result_dict.items() if k in keys}
+  agg_dict = {k: [fn(np.array(arrays)) for fn in agg_fns] for k, arrays in result_dict.items()}
+  return agg_dict
+
+
+def plot_ece_one_method(model_key: ModelKey, result_dict: RESULT_DICT, ax=None, n_windows=100, plt_kwargs={}):
   if not ax:
     plt.figure()
     ax = plt.gca()
   # compute mean and std ece curves for all seeds
+  ece_data = [compute_data_for_ece_plot(y_true=y_true, y_pred=y_pred, n_windows=n_windows)
+              for y_true, y_pred in zip(result_dict["y_true"], result_dict["y_pred"])]
+  arrays = {key: np.stack([d[key] for d in ece_data]) for key in ece_data[0].keys()}
+  mean = {k: np.mean(array, axis=0) for k, array in arrays.items()}
+  std = {k: np.std(array, axis=0) for k, array in arrays.items()}
   # plot them on an axes
-  raise NotImplementedError
+  plot_ece(ax=ax, label=model_key.model_type, **mean, **plt_kwargs)
+  return ax
 
 
-def mean(inputs):
+def mean_default_zero(inputs):
   """Be able to take the mean of an empty array without hitting NANs."""
   # pylint disable necessary for numpy and pandas
   if len(inputs) == 0:  # pylint: disable=g-explicit-length-test
@@ -220,21 +258,37 @@ def verify_probability_shapes(probs):
 
 
 def plot_predictive_entropy_histogram_all_methods(data_result_dict: DATA_RESULT_DICT):
+  transposed = transpose_dict(data_result_dict)
   return grid_plot_wrapper(
     fn=plot_predictive_entropy_histogram_one_method,
     ncols=3,
-    n_plots=len(data_result_dict["in_domain_test"]),
-    get_kwargs=lambda i: data_result_dict,
+    n_plots=len(transposed),
+    get_kwargs=[],
   )
 
 
-def plot_predictive_entropy_histogram_one_method(data_result_dict: DATA_RESULT_DICT, ax=None):
+def transpose_dict(d: Dict[Any, Dict[Any, Any]]):
+  new_d = defaultdict(dict)
+  for k1, v1 in d.items():
+    for k2, v2 in v1.items():
+      assert k1 not in new_d[k2]
+      new_d[k2][k1] = v2
+  return new_d
+
+
+def plot_predictive_entropy_histogram_one_method(model_key: ModelKey,
+                                                 domain_result_dict: Dict, ax=None):
   if not ax:
     plt.figure()
     ax = plt.gca()
+  model_type = model_key.model_type
   # draw histogram for ID
+  mean_y_total_uncert = np.mean(domain_result_dict["in_domain_test"]["y_total_uncert"], axis=0)
+  plot_predictive_entropy_line_histogram(mean_y_total_uncert, ax=ax, color="red", title=f"Predictive Entropy {model_type}")
   # draw histogram for OOD
-  raise NotImplementedError
+  mean_y_total_uncert = np.mean(domain_result_dict["ood_test"]["y_total_uncert"], axis=0)
+  plot_predictive_entropy_line_histogram(mean_y_total_uncert, ax=ax, color="blue", title=f"Predictive Entropy {model_type}")
+  return ax
 
 
 def line_hist(array, thresholds, ax=None, **kwargs):
@@ -262,6 +316,7 @@ def plot_predictive_entropy_line_histogram(
         y_total_uncert,
         start=-0.01, end=2.5, step_size=0.1, ax=None,
         color="blue",
+        title="",
         **kwargs):
   if not ax:
     plt.figure()
@@ -270,6 +325,7 @@ def plot_predictive_entropy_line_histogram(
   line_hist(y_total_uncert, thresholds, ax=ax, color=color, **kwargs)
   ax.set_xlabel("Entropy (nats)")
   ax.set_ylabel("# of Examples")
+  ax.set_title(title)
   # ax.legend(loc='upper left', facecolor='white', fontsize='small')
   return ax
 

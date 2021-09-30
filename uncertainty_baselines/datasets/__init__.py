f0a590d08dc430e82c13f27abfe0c4e74951d0ef
Zachary Nado <znado@google.com>
2021-09-29 18:25:37 -0700

    Making dataset import error messages clearer.
    PiperOrigin-RevId: 399823421

--- uncertainty_baselines/datasets/__init__.py
+++ uncertainty_baselines/datasets/__init__.py
@@ -60,14 +60,23 @@ from uncertainty_baselines.datasets.toxic_comments import WikipediaToxicityDatas
 # pylint: enable=g-bad-import-order
 
 try:
-  # Try to import datasets depending on librosa.
   from uncertainty_baselines.datasets.smcalflow import MultiWoZDataset  # pylint: disable=g-import-not-at-top
   from uncertainty_baselines.datasets.smcalflow import SMCalflowDataset  # pylint: disable=g-import-not-at-top
+except ImportError:
+  logging.warning(
+      'Skipped importing the SMCalflow dataset due to ImportError. Try '
+      'installing uncertainty baselines with the `datasets` extras.',
+      exc_info=True)
+
+try:
+  # Try to import datasets depending on librosa.
   from uncertainty_baselines.datasets.speech_commands import SpeechCommandsDataset  # pylint: disable=g-import-not-at-top
 except ImportError:
   logging.warning(
-      'Skipped dataset due to ImportError. Try installing '
-      'uncertainty baselines with the `datasets` extras.', exc_info=True)
+      'Skipped importing the Speech Commands dataset due to ImportError. Try '
+      'installing uncertainty baselines with the `datasets` extras.',
+      exc_info=True)
 except OSError:
   logging.warning(
-      'Skipped Speech Commands dataset due to OSError.', exc_info=True)
+      'Skipped importing the Speech Commands dataset due to OSError.',
+      exc_info=True)

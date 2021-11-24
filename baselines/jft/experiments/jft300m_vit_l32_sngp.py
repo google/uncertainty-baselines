7af4fbbda0267164a7e47c5504ad395a2047aafd
Mark Collier <markcollier@google.com>
2021-11-24 01:43:24 -0800

    Internal change
    PiperOrigin-RevId: 411994921

---
+++ baselines/jft/experiments/jft300m_vit_l32_sngp.py
@@ -0,0 +1,103 @@
+# coding=utf-8
+# Copyright 2021 The Uncertainty Baselines Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# pylint: disable=line-too-long
+r"""ViT-SNGP L/32.
+
+"""
+# pylint: enable=line-too-long
+
+import ml_collections
+import common_fewshot  # local file import
+
+
+def get_config():
+  """Config for training a patch-transformer on JFT."""
+  config = ml_collections.ConfigDict()
+
+  # Directory for the version de-dup'd from BiT downstream test-sets.
+  config.dataset = 'jft/entity:1.0.0'
+  config.val_split = 'test[:49511]'  # aka tiny_test/test[:5%] in task_adapt
+  config.train_split = 'train'  # task_adapt used train+validation so +64167
+  config.num_classes = 18291
+  config.init_head_bias = -10.0
+
+  config.trial = 0
+  config.batch_size = 4096
+  config.num_epochs = 7
+
+  pp_common = '|value_range(-1, 1)'
+  pp_common += f'|onehot({config.num_classes})'
+  # To use ancestor 'smearing', use this line instead:
+  # pp_common += f'|onehot({config.num_classes}, key='labels_extended', key_result='labels')  # pylint: disable=line-too-long
+  pp_common += '|keep(["image", "labels"])'
+  config.pp_train = 'decode_jpeg_and_inception_crop(224)|flip_lr' + pp_common
+  config.pp_eval = 'decode|resize_small(256)|central_crop(224)' + pp_common
+  config.shuffle_buffer_size = 250_000  # Per host, so small-ish is ok.
+
+  config.log_training_steps = 50
+  config.log_eval_steps = 1000
+  # NOTE: Save infrequently to prevent crowding the disk space.
+  config.checkpoint_steps = 17250
+  config.checkpoint_timeout = 10
+
+  # Model section
+  config.model = ml_collections.ConfigDict()
+  config.model.patches = ml_collections.ConfigDict()
+  config.model.patches.size = [32, 32]
+  config.model.hidden_size = 1024
+  config.model.transformer = ml_collections.ConfigDict()
+  config.model.transformer.attention_dropout_rate = 0.
+  config.model.transformer.dropout_rate = 0.
+  config.model.transformer.mlp_dim = 4096
+  config.model.transformer.num_heads = 16
+  config.model.transformer.num_layers = 24
+  config.model.classifier = 'token'  # Or 'gap'
+  config.model.representation_size = None
+
+  # Gaussian process layer section
+  config.gp_layer = ml_collections.ConfigDict()
+  # Use momentum-based (i.e., non-exact) covariance update for pre-training.
+  # This is because the exact covariance update can be unstable for pretraining,
+  # since it involves inverting a precision matrix accumulated over 300M data.
+  config.gp_layer.covmat_momentum = .999
+  config.gp_layer.ridge_penalty = 1.
+  # No need to use mean field adjustment for pretraining.
+  config.gp_layer.mean_field_factor = -1.
+
+  # Optimizer section
+  config.optim_name = 'Adam'
+  config.optim = ml_collections.ConfigDict()
+  config.optim.weight_decay = 0.1
+  config.optim.beta1 = 0.9
+  config.optim.beta2 = 0.999
+  config.weight_decay = None  # No explicit weight decay
+
+  # TODO(lbeyer): make a mini-language like preprocessings.
+  config.lr = ml_collections.ConfigDict()
+  # LR has to be lower for GP layer and on larger models.
+  config.lr.base = 6e-4
+  config.lr.warmup_steps = 10_000
+  config.lr.decay_type = 'linear'
+  config.lr.linear_end = 1e-5
+
+  # Few-shot eval section
+  config.fewshot = common_fewshot.get_fewshot()
+  config.fewshot.log_steps = 25_000
+
+  config.args = {}
+  return config
+
+

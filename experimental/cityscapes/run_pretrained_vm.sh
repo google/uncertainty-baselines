b7310327f39a722746a9d7ed50b18b278fad500b
ekellbuch <ekellbuchanan@gmail.com>
2021-11-30 17:05:18 +0000

    update config files for pretrained weights run in vm

---
+++ experimental/cityscapes/run_pretrained_vm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# train cityscapes using segmenter with pretrained backbone
+# supports 2 options to
+
+declare -A configfiles=( ["deterministic"]="experiments/imagenet21k_segmenter_cityscapes.py" ["sngp"]="experiments/imagenet21k_segmenter_cityscapes_sngp.py" )
+
+num_cores=8
+tpu='local'
+use_gpu=False
+
+for config_mode in "sngp" #"deterministic" #"sngp"
+do
+config_file="${configfiles[$config_mode]}"
+#output_dir="/home/ekellbuch/ub_ekb/experimental/cityscapes/outputs13"
+output_dir="gs://ub-ekb/segmenter/cityscapes/run0/$config_mode"
+echo "${output_dir} ${config_file}"
+python3 deterministic.py --output_dir=${output_dir} \
+	--num_cores=$num_cores \
+	--use_gpu=$use_gpu \
+	--config=${config_file} \
+	--tpu=$tpu
+
+done

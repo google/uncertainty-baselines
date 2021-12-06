8e7761a59e8f8fffbaf3f4e6119366e4f344069c
ekellbuch <ekellbuchanan@gmail.com>
2021-12-06 22:44:24 +0000

    update config files for 512 run'

--- experimental/cityscapes/run_pretrained_vm512.sh
+++ experimental/cityscapes/run_pretrained_vm512.sh
@@ -3,13 +3,13 @@
 # train cityscapes using segmenter with pretrained backbone
 # supports options to try 
 
-declare -A configfiles=( ["deterministic"]="experiments/imagenet21k_segmenter_cityscapes_larger1.py" ["sngp"]="experiments/imagenet21k_segmenter_cityscapes_sngp.py" ["scratch"]="experiments/segmenter_cityscapes.py")
+declare -A configfiles=( ["deterministic"]="experiments/imagenet21k_segmenter_cityscapes512.py" ["sngp"]="experiments/imagenet21k_segmenter_cityscapes_sngp512.py" ["scratch"]="experiments/segmenter_cityscapes512.py")
 
 num_cores=8
 tpu='local'
 use_gpu=False
 
-for config_mode in "deterministic" # "scratch"  "sngp" 
+for config_mode in "sngp" "scratch" #"deterministic" # "scratch"  "sngp" 
 do
 config_file="${configfiles[$config_mode]}"
 output_dir="gs://ub-ekb/segmenter/cityscapes/run3/$config_mode"

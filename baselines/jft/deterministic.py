67540e49bed5573254c148f728c0f093c1956847
Mark Collier <markcollier@google.com>
2021-12-07 08:49:03 -0800

    Evaluate Imagenet-21k pre-trained models on Imagenet-1k without fine-tuning.
    Deterministic ViT-B/16, accuracy: 78.03
    
    PiperOrigin-RevId: 414737632

--- baselines/jft/deterministic.py
+++ baselines/jft/deterministic.py
@@ -284,6 +284,9 @@ def main(config, output_dir):
     logits, out = model.apply({'params': flax.core.freeze(params)},
                               images,
                               train=False)
+    label_indices = config.get('label_indices')
+    if label_indices:
+      logits = logits[:, label_indices]
 
     # Note that logits and labels are usually of the shape [batch,num_classes].
     # But for OOD data, when num_classes_ood > num_classes_ind, we need to
@@ -291,7 +294,9 @@ def main(config, output_dir):
     # logits. That is just to avoid shape mismatch. The output losses does not
     # have any meaning for OOD data, because OOD not belong to any IND class.
     losses = getattr(train_utils, config.get('loss', 'sigmoid_xent'))(
-        logits=logits, labels=labels[:, :config.num_classes], reduction=False)
+        logits=logits,
+        labels=labels[:, :(len(label_indices) if label_indices
+                           else config.num_classes)], reduction=False)
     loss = jax.lax.psum(losses * mask, axis_name='batch')
 
     top1_idx = jnp.argmax(logits, axis=1)
@@ -309,6 +314,9 @@ def main(config, output_dir):
     logits, out = model.apply({'params': flax.core.freeze(params)},
                               images,
                               train=False)
+    label_indices = config.get('label_indices')
+    if label_indices:
+      logits = logits[:, label_indices]
 
     losses = getattr(train_utils, config.get('loss', 'softmax_xent'))(
         logits=logits, labels=labels, reduction=False)
@@ -367,6 +375,9 @@ def main(config, output_dir):
       logits, _ = model.apply(
           {'params': flax.core.freeze(params)}, images,
           train=True, rngs={'dropout': rng_model_local})
+      label_indices = config.get('label_indices')
+      if label_indices:
+        logits = logits[:, label_indices]
       return getattr(train_utils, config.get('loss', 'sigmoid_xent'))(
           logits=logits, labels=labels)
 
@@ -399,13 +410,16 @@ def main(config, output_dir):
     return opt, l, rng, measurements
 
   rng, train_loop_rngs = jax.random.split(rng)
+  reint_params = ('head/kernel', 'head/bias')
+  if config.get('only_eval', False):
+    reint_params = []
   checkpoint_data = checkpoint_utils.maybe_load_checkpoint(
       train_loop_rngs=train_loop_rngs,
       save_checkpoint_path=save_checkpoint_path,
       init_optimizer=opt_cpu,
       init_params=params_cpu,
       init_fixed_model_states=None,
-      default_reinit_params=('head/kernel', 'head/bias'),
+      default_reinit_params=reint_params,
       config=config,
   )
   train_loop_rngs = checkpoint_data.train_loop_rngs
@@ -481,18 +495,19 @@ def main(config, output_dir):
       range(first_step + 1, total_steps + 1), train_iter, lr_iter):
 
     with jax.profiler.TraceAnnotation('train_step', step_num=step, _r=1):
-      opt_repl, loss_value, train_loop_rngs, extra_measurements = update_fn(
-          opt_repl,
-          lr_repl,
-          train_batch['image'],
-          train_batch['labels'],
-          rng=train_loop_rngs)
+      if not config.get('only_eval', False):
+        opt_repl, loss_value, train_loop_rngs, extra_measurements = update_fn(
+            opt_repl,
+            lr_repl,
+            train_batch['image'],
+            train_batch['labels'],
+            rng=train_loop_rngs)
 
     if jax.process_index() == 0:
       profiler(step)
 
     # Checkpoint saving
-    if train_utils.itstime(
+    if not config.get('only_eval', False) and train_utils.itstime(
         step, config.get('checkpoint_steps'), total_steps, process=0):
       write_note('Checkpointing...')
       chrono.pause()
@@ -524,7 +539,7 @@ def main(config, output_dir):
       chrono.resume()
 
     # Report training progress
-    if train_utils.itstime(
+    if not config.get('only_eval', False) and train_utils.itstime(
         step, config.log_training_steps, total_steps, process=0):
       write_note('Reporting training progress...')
       train_loss = loss_value[0]  # Keep to return for reproducibility tests.

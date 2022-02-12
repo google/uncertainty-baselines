ce91e3d21c8505b8f6dc7cff9ecc2779bc8fc21b
Kelly Buchanan <ekellbuchanan@gmail.com>
2022-02-11 19:01:11 -0500

    remove stochastic_depth_layer flag to integrate vit and segmenter code

--- uncertainty_baselines/models/segmenter.py
+++ uncertainty_baselines/models/segmenter.py
@@ -23,235 +23,12 @@ import flax.linen as nn
 import jax
 import jax.numpy as jnp
 import ml_collections
+from uncertainty_baselines.models import vit
 
 Array = Any
 PRNGKey = Any
 Shape = Tuple[int]
 Dtype = Any
-
-
-class IdentityLayer(nn.Module):
-  """Identity layer, convenient for giving a name to an array."""
-
-  @nn.compact
-  def __call__(self, x):
-    return x
-
-
-class AddPositionEmbs(nn.Module):
-  """Adds (optionally learned) positional embeddings to the inputs.
-
-  Attributes:
-    posemb_init: positional embedding initializer.
-  """
-  # TODO(kellybuchanan): check initialization,
-  # nn.initializers.normal(stddev=0.02) from BERT.
-  posemb_init: Callable[[PRNGKey, Shape, Dtype], Array]
-
-  @nn.compact
-  def __call__(self, inputs):
-    """Applies AddPositionEmbs module.
-
-    By default this layer uses a fixed sinusoidal embedding table. If a
-    learned position embedding is desired, pass an initializer to
-    posemb_init.
-
-    Args:
-      inputs: Inputs to the layer.
-
-    Returns:
-      Output tensor with shape `(bs, timesteps, in_dim)`.
-    """
-    # inputs.shape is (batch_size, seq_len, emb_dim).
-    assert inputs.ndim == 3, ('Number of dimensions should be 3,'
-                              ' but it is: %d' % inputs.ndim)
-    pos_emb_shape = (1, inputs.shape[1], inputs.shape[2])
-    pe = self.param('pos_embedding', self.posemb_init, pos_emb_shape)
-    return inputs + pe
-
-
-class MlpBlock(nn.Module):
-  """Transformer MLP / feed-forward block."""
-
-  mlp_dim: int
-  dtype: Dtype = jnp.float32
-  out_dim: Optional[int] = None
-  dropout_rate: float = 0.1
-  kernel_init: Callable[[PRNGKey, Shape, Dtype],
-                        Array] = nn.initializers.xavier_uniform()
-  bias_init: Callable[[PRNGKey, Shape, Dtype],
-                      Array] = nn.initializers.normal(stddev=1e-6)
-  use_bias: bool = True
-  precision: Optional[jax.lax.Precision] = None
-  activation_fn: Callable[[jnp.ndarray], jnp.ndarray] = nn.gelu
-
-  @nn.compact
-  def __call__(self, inputs, *, deterministic):
-    """Applies Transformer MlpBlock module."""
-    actual_out_dim = inputs.shape[-1] if self.out_dim is None else self.out_dim
-    x = nn.Dense(
-        features=self.mlp_dim,
-        dtype=self.dtype,
-        use_bias=self.use_bias,
-        kernel_init=self.kernel_init,
-        bias_init=self.bias_init,
-        precision=self.precision)(  # pytype: disable=wrong-arg-types
-            inputs)
-    x = self.activation_fn(x)
-    x = nn.Dropout(rate=self.dropout_rate)(x, deterministic=deterministic)
-    output = nn.Dense(
-        features=actual_out_dim,
-        dtype=self.dtype,
-        kernel_init=self.kernel_init,
-        bias_init=self.bias_init,
-        use_bias=self.use_bias,
-        precision=self.precision)(  # pytype: disable=wrong-arg-types
-            x)
-    output = nn.Dropout(
-        rate=self.dropout_rate)(
-            output, deterministic=deterministic)
-    return output
-
-
-class Encoder1DBlock(nn.Module):
-  """Transformer encoder layer.
-
-  Attributes:
-    inputs: input data.
-    mlp_dim: dimension of the mlp on top of attention block.
-    dtype: the dtype of the computation (default: float32).
-    dropout_rate: dropout rate.
-    attention_dropout_rate: dropout for attention heads.
-    deterministic: bool, deterministic or not (to apply dropout).
-    num_heads: Number of heads in nn.MultiHeadDotProductAttention
-    stochastic_depth: probability of dropping a layer linearly grows from 0 to
-      the provided value.
-
-  """
-
-  mlp_dim: int
-  num_heads: int
-  dtype: Dtype = jnp.float32
-  dropout_rate: float = 0.1
-  attention_dropout_rate: float = 0.1
-  stochastic_depth: float = 0.0
-
-  def get_stochastic_depth_mask(self, x: jnp.ndarray,
-                                deterministic: bool) -> jnp.ndarray:
-    """Generate the stochastic depth mask in order to apply layer-drop.
-
-    Args:
-      x: Input tensor.
-      deterministic: Weather we are in the deterministic mode (e.g inference
-        time) or not.
-
-    Returns:
-      Stochastic depth mask.
-    """
-    if not deterministic and self.stochastic_depth:
-      shape = (x.shape[0],) + (1,) * (x.ndim - 1)
-      return jax.random.bernoulli(
-          self.make_rng('dropout'), self.stochastic_depth, shape)
-    else:
-      return 0.0
-
-  @nn.compact
-  def __call__(self, inputs, *, deterministic):
-    """Applies Encoder1DBlock module.
-
-    Args:
-      inputs: Inputs to the layer.
-      deterministic: Dropout will not be applied when set to true.
-
-    Returns:
-      output after transformer encoder block.
-    """
-
-    # Attention block.
-    assert inputs.ndim == 3, f'Expected (batch, seq, hidden) got {inputs.shape}'
-    x = nn.LayerNorm(dtype=self.dtype, name='LayerNorm_0')(inputs)
-    x = nn.MultiHeadDotProductAttention(
-        dtype=self.dtype,
-        kernel_init=nn.initializers.xavier_uniform(),
-        broadcast_dropout=False,
-        deterministic=deterministic,
-        dropout_rate=self.attention_dropout_rate,
-        num_heads=self.num_heads,
-        name='MultiHeadDotProductAttention_1')(x, x)
-    x = nn.Dropout(rate=self.dropout_rate)(x, deterministic=deterministic)
-    x = x * (1.0 - self.get_stochastic_depth_mask(x, deterministic)) + inputs
-
-    # MLP block.
-    y = nn.LayerNorm(dtype=self.dtype, name='LayerNorm_2')(x)
-    y = MlpBlock(
-        mlp_dim=self.mlp_dim,
-        dtype=self.dtype,
-        name='MlpBlock_3',
-        dropout_rate=self.dropout_rate,
-        activation_fn=nn.gelu,
-        kernel_init=nn.initializers.xavier_uniform(),
-        bias_init=nn.initializers.normal(stddev=1e-6))(
-            y, deterministic=deterministic)
-
-    return y * (1.0 - self.get_stochastic_depth_mask(x, deterministic)) + x
-
-
-class Encoder(nn.Module):
-  """Transformer Model Encoder for sequence to sequence translation.
-
-  Attributes:
-    num_layers: number of layers
-    mlp_dim: dimension of the mlp on top of attention block
-    num_heads: Number of heads in nn.MultiHeadDotProductAttention
-    dropout_rate: dropout rate.
-    attention_dropout_rate: dropout rate in self attention.
-    stochastic_depth: probability of dropping a layer linearly grows from 0 to
-    the provided value. Our implementation of stochastic depth follows timm
-    library, which does per-example layer dropping and uses independent
-    dropping patterns for each skip-connection.
-    dtype: Dtype of activations.
-  """
-
-  num_layers: int
-  mlp_dim: int
-  num_heads: int
-  dropout_rate: float = 0.1
-  attention_dropout_rate: float = 0.1
-  stochastic_depth: float = 0.0
-  dtype: Any = jnp.float32
-
-  @nn.compact
-  def __call__(self, inputs: jnp.ndarray, *, train: bool = False):
-    """Applies Transformer model on the inputs.
-
-    Args:
-      inputs: Inputs to the layer.
-      train: Set to `True` when training.
-
-    Returns:
-      output of a transformer encoder.
-    """
-    assert inputs.ndim == 3  # (batch, len, emb)
-    x = AddPositionEmbs(
-        posemb_init=nn.initializers.normal(stddev=0.02),  # from BERT.
-        name='posembed_input')(
-            inputs)
-    x = nn.Dropout(rate=self.dropout_rate)(x, deterministic=not train)
-
-    # Input Encoder
-    for lyr in range(self.num_layers):
-      x = Encoder1DBlock(
-          mlp_dim=self.mlp_dim,
-          dropout_rate=self.dropout_rate,
-          attention_dropout_rate=self.attention_dropout_rate,
-          stochastic_depth=(lyr / max(self.num_layers - 1, 1)) *
-          self.stochastic_depth,
-          name=f'encoderblock_{lyr}',
-          num_heads=self.num_heads)(
-              x, deterministic=not train)
-    encoded = nn.LayerNorm(name='encoder_norm')(x)
-
-    return encoded
 
 
 class ViTBackbone(nn.Module):
@@ -299,7 +76,7 @@ class ViTBackbone(nn.Module):
       cls = jnp.tile(cls, [n, 1, 1])
       x = jnp.concatenate([cls, x], axis=1)
 
-    x = Encoder(name='Transformer',
+    x = vit.Encoder(name='Transformer',
                 mlp_dim=self.mlp_dim,
                 num_layers=self.num_layers,
                 num_heads=self.num_heads,
@@ -337,7 +114,7 @@ class SegVit(nn.Module):
           hidden_size=self.backbone_configs.hidden_size,
           dropout_rate=self.backbone_configs.dropout_rate,
           attention_dropout_rate=self.backbone_configs.attention_dropout_rate,
-          classifier='gap',
+          classifier=self.backbone_configs.classifier,
           name='backbone')(
               x, train=train)
     else:

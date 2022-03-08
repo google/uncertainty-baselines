Fix pre_logits computation for BatchEnsemble.
It erroneously computed log 1/ens_size sum softmax(pre_logits). Following how representation_fn is computed for linear few-shot, we reshape pre_logits from [batch * ens_size, hidden_size] to [batch, hidden_size * ens_size]. This way representation-based methods operate on the full set of outputs.

See thread in AL OSS Infra chatroom. This was a bug for a while, affecting Maha in OOD detection and density acquisition in AL.

PiperOrigin-RevId: 433330895

baselines/jft/active_learning.py
@@ -107,7 +107,6 @@ def get_ids_logits_masks(*,
     logits, out = model.apply({'params': flax.core.freeze(params)},
                               images,
                               train=False)
-    pre_logits = out['pre_logits']
     if config and config.model_type == 'batchensemble':
       ens_size = config.model.transformer.ens_size
       loss_name = config.get('loss', 'sigmoid_xent')
@@ -115,17 +114,15 @@ def get_ids_logits_masks(*,
       if loss_name == 'sigmoid_xent':
         if average_logits:
           logits = batchensemble_utils.log_average_sigmoid_probs(logits)
-        pre_logits = batchensemble_utils.log_average_sigmoid_probs(
-            jnp.asarray(jnp.split(out['pre_logits'], ens_size)))
       elif loss_name == 'softmax_xent':
         if average_logits:
           logits = batchensemble_utils.log_average_softmax_probs(logits)
-        pre_logits = batchensemble_utils.log_average_softmax_probs(
-            jnp.asarray(jnp.split(out['pre_logits'], ens_size)))
       else:
         raise ValueError(f'Loss name: {loss_name} not supported.')
 
     if use_pre_logits:
+      pre_logits = jnp.concatenate(
+          jnp.split(out['pre_logits'], ens_size), axis=-1)
       output = pre_logits
     else:
       output = logits

baselines/jft/batchensemble.py
@@ -290,13 +290,11 @@ def main(config, output_dir):
     if loss_name == 'sigmoid_xent':
       ens_logits = batchensemble_utils.log_average_sigmoid_probs(
           jnp.asarray(jnp.split(tiled_logits, ens_size)))
-      pre_logits = batchensemble_utils.log_average_sigmoid_probs(
-          jnp.asarray(jnp.split(out['pre_logits'], ens_size)))
     else:  # softmax
       ens_logits = batchensemble_utils.log_average_softmax_probs(
           jnp.asarray(jnp.split(tiled_logits, ens_size)))
-      pre_logits = batchensemble_utils.log_average_softmax_probs(
-          jnp.asarray(jnp.split(out['pre_logits'], ens_size)))
+    pre_logits = jnp.concatenate(
+        jnp.split(out['pre_logits'], ens_size), axis=-1)
 
     losses = getattr(train_utils, loss_name)(
         logits=ens_logits,
@@ -323,13 +321,11 @@ def main(config, output_dir):
     if loss_name == 'sigmoid_xent':
       ens_logits = batchensemble_utils.log_average_sigmoid_probs(
           jnp.asarray(jnp.split(tiled_logits, ens_size)))
-      pre_logits = batchensemble_utils.log_average_sigmoid_probs(
-          jnp.asarray(jnp.split(out['pre_logits'], ens_size)))
     else:  # softmax
       ens_logits = batchensemble_utils.log_average_softmax_probs(
           jnp.asarray(jnp.split(tiled_logits, ens_size)))
-      pre_logits = batchensemble_utils.log_average_softmax_probs(
-          jnp.asarray(jnp.split(out['pre_logits'], ens_size)))
+    pre_logits = jnp.concatenate(
+        jnp.split(out['pre_logits'], ens_size), axis=-1)
 
     label_indices = config.get('label_indices')
     if label_indices:

baselines/jft/batchensemble_utils.py
@@ -338,13 +338,11 @@ def create_evaluation_fn(model, config):
     if loss_name == 'sigmoid_xent':
       ens_logits = log_average_sigmoid_probs(
           jnp.asarray(jnp.split(tiled_logits, ens_size)))
-      pre_logits = log_average_sigmoid_probs(
-          jnp.asarray(jnp.split(out['pre_logits'], ens_size)))
     else:  # softmax
       ens_logits = log_average_softmax_probs(
           jnp.asarray(jnp.split(tiled_logits, ens_size)))
-      pre_logits = log_average_softmax_probs(
-          jnp.asarray(jnp.split(out['pre_logits'], ens_size)))
+    pre_logits = jnp.concatenate(
+        jnp.split(out['pre_logits'], ens_size), axis=-1)
 
     losses = getattr(train_utils, loss_name)(
         logits=ens_logits,

uncertainty_baselines/models/vit_hetgpbe.py
@@ -192,16 +192,11 @@ class VisionTransformerHetGPBE(nn.Module):
       if self.multiclass:
         logits = log_average_softmax_probs(
             jnp.asarray(jnp.split(logits, self.transformer.get('ens_size'))))
-        out['pre_ens_logits'] = out['pre_logits']
-        out['pre_logits'] = log_average_softmax_probs(
-            jnp.asarray(jnp.split(out['pre_logits'],
-                                  self.transformer.get('ens_size'))))
       else:
         logits = log_average_sigmoid_probs(
             jnp.asarray(jnp.split(logits, self.transformer.get('ens_size'))))
-        out['pre_ens_logits'] = out['pre_logits']
-        out['pre_logits'] = log_average_sigmoid_probs(
-            jnp.asarray(jnp.split(out['pre_logits'],
-                                  self.transformer.get('ens_size'))))
+      out['pre_logits'] = jnp.concatenate(
+          jnp.split(out['pre_logits'], self.transformer.get('ens_size')),
+          axis=-1)
 
     return logits, out
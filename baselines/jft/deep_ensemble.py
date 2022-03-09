Fix prelogit computation in deep ensembles.
PiperOrigin-RevId: 433532503

baselines/jft/deep_ensemble.py
@@ -95,7 +95,7 @@ def ensemble_prediction_fn(model_apply_fn, params, images, loss_as_str):
   ens_logits = ens_logits_fn(ens_logits)
 
   ens_prelogits = jnp.asarray([out['pre_logits'] for _, out in outputs])
-  ens_prelogits = ens_logits_fn(ens_prelogits)
+  ens_prelogits = jnp.concatenate(ens_prelogits, axis=-1)
 
   return ens_logits, ens_prelogits
 

baselines/jft/deep_ensemble_test.py
@@ -68,9 +68,10 @@ class DeepEnsembleTest(parameterized.TestCase):
       link_fn = jax.nn.sigmoid
 
     expected_probs = jnp.mean(link_fn(raw_logits), axis=0)
+    expected_pre_logits = jnp.concatenate(raw_logits, axis=-1)
     np.testing.assert_allclose(link_fn(actual_logits), expected_probs,
                                rtol=1e-06, atol=1e-06)
-    np.testing.assert_allclose(link_fn(actual_pre_logits), expected_probs,
+    np.testing.assert_allclose(actual_pre_logits, expected_pre_logits,
                                rtol=1e-06, atol=1e-06)
 
 if __name__ == '__main__':
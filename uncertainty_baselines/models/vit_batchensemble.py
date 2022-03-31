538d90a9350b89cbb137c777c15f02f1701181ed
Mike Dusenberry <dusenberrymw@google.com>
2022-03-31 09:00:47 -0700

    Removes an outdated `load` method from the ViT BatchEnsemble models.
    PiperOrigin-RevId: 438580505

--- uncertainty_baselines/models/vit_batchensemble.py
+++ uncertainty_baselines/models/vit_batchensemble.py
@@ -17,17 +17,11 @@
 
 from typing import Any, Callable, Iterable, Mapping, Optional, Sequence, Tuple
 
-from absl import logging
 import edward2.jax as ed
-import flax
 import flax.linen as nn
-import jax
 import jax.numpy as jnp
-import numpy as np
-import scipy
 
 from uncertainty_baselines.models import vit
-
 
 DType = type(jnp.float32)
 InitializeFn = Callable[[jnp.ndarray, Iterable[int], DType], jnp.ndarray]
@@ -250,83 +244,6 @@ class PatchTransformerBE(nn.Module):
   transformer: Optional[Params] = None
   classifier: str = "token"
   head_kernel_init: InitializeFn = nn.initializers.zeros
-
-  @classmethod
-  def load(
-      cls,
-      prefix: str,
-      init_params: Mapping[str, Any],
-      model_params: Mapping[str, Any],
-      partition_specs: Sequence[Any],
-      keep_head: bool = False,
-  ) -> Mapping[str, Any]:
-    """Loads from Transformer checkpoint except head parameters.
-
-    Args:
-      prefix: Prefix of the model checkpoint to use.
-      init_params: Dictionary with unreplicated parameters of the new model.
-      model_params: Dictionary with the configuration of the new model.
-      partition_specs: A sequence of PartitionSpecs. They map expert parameter
-        names (RegEx patterns) to a TPU layout. Expected to be None or empty.
-      keep_head: bool, whether head must be kept or replaced with a random one.
-
-    Returns:
-      A new dictionary of params to replace init_params.
-    """
-    local_devices = sorted(jax.local_devices(), key=lambda device: device.id)
-    if partition_specs:
-      raise ValueError("Partition specs cannot be used for Batchensemble.")
-    restored = None
-    if restored is None:
-      raise ValueError(f"No valid checkpoints with prefix {prefix!r}")
-    # Checkpoints contain FrozenDicts, which are immutable.
-    restored_params = flax.core.unfreeze(restored["target"])
-    # The following allows implementing both fine-tuning head variants from
-    # https://docs.google.com/presentation/d/1mWGpOoCq1TGESg7ZpQwBIxBpEQxWk9cjfeVS_qQi1Gc/edit#slide=id.g9798de2d4d_2_0
-    # depending on the value of `representation_size` in the fine-tuning job:
-    # - `None` is variant 3 (c-head): drop the whole head and add a nn.Linear.
-    # - same number as in pre-training means variant 1 (a-head): keep the head
-    #   but reset the last layer (logits) for the new task.
-    if model_params["representation_size"] is None:
-      if "pre_logits" in restored_params:
-        logging.info("Resformer: drop-head variant")
-        del restored_params["pre_logits"]
-    if not keep_head:
-      restored_params["batchensemble_head"]["kernel"] = np.stack(
-          [init_params["batchensemble_head"]["kernel"]] * len(local_devices))
-      restored_params["batchensemble_head"]["bias"] = np.stack(
-          [init_params["batchensemble_head"]["bias"]] * len(local_devices))
-    # The following implements "high-res finetuning" for transformer models.
-    if "posembed_input" in restored_params.get("Transformer", {}):
-      # Rescale the grid of position embeddings. Param shape is (1,N,rep.size)
-      posemb = (
-          restored_params["Transformer"]["posembed_input"]["pos_embedding"][0])
-      posemb_new = init_params["Transformer"]["posembed_input"]["pos_embedding"]
-      if posemb.shape != posemb_new.shape:
-        logging.info("Resformer: resized variant: %s to %s", posemb.shape,
-                     posemb_new.shape)
-        ntok_new = posemb_new.shape[1]
-
-        if (model_params.get("cls_token", False) or
-            model_params.get("classifier", None) == "token"):
-          posemb_tok, posemb_grid = posemb[:, :1], posemb[0, 1:]
-          ntok_new -= 1
-        else:
-          posemb_tok, posemb_grid = posemb[:, :0], posemb[0]
-
-        gs_old = int(np.sqrt(len(posemb_grid)))
-        gs_new = int(np.sqrt(ntok_new))
-        logging.info("Resformer: grid-size from %s to %s", gs_old, gs_new)
-        posemb_grid = posemb_grid.reshape(gs_old, gs_old, -1)
-
-        zoom = (gs_new / gs_old, gs_new / gs_old, 1)
-        posemb_grid = scipy.ndimage.zoom(posemb_grid, zoom, order=1)
-        posemb_grid = posemb_grid.reshape(1, gs_new * gs_new, -1)
-        posemb = jnp.array(np.concatenate([posemb_tok, posemb_grid], axis=1))
-        restored_params["Transformer"]["posembed_input"][
-            "pos_embedding"] = np.stack([posemb] * len(local_devices))
-
-    return flax.core.freeze(restored_params)
 
   def embed(self,
             images: jnp.ndarray,

0a325d3ae10a9f44ff05495f09bf6294efad8c55
Zelda Mariet <zmariet@google.com>
2022-04-06 11:24:50 -0700

    Clean up, generalize, and test the code that pre-processes the RDL experimental results.
    In particular:
      * Filling in missing metrics for upstream datasets.
      * Aggregating metrics
      * Filtering fewshot and compute metrics based on upstream dataset.
    
    PiperOrigin-RevId: 439891556

--- experimental/big_paper/colab_utils.py
+++ experimental/big_paper/colab_utils.py
@@ -15,15 +15,62 @@
 
 """Utility functions used to process xmanager experiments."""
 
-from typing import Any, Dict, Iterable, List
+import itertools
+import re
+from typing import Any, Dict, Iterable, List, Optional
 
 import pandas as pd
-
 
 _HPARAM_PREFIX = 'config.'
 _RANDOM_SEED_COL = _HPARAM_PREFIX + 'seed'
 _DATASET_COL = _HPARAM_PREFIX + 'dataset'
 _MODEL_COL = 'model'
+
+# In-distribution metrics across train/validation/test splits.
+_SPLIT_METRICS = ('loss', 'prec@1', 'ece', 'calib_auc')
+# Metrics specific to the diabetic retinopathy dataset.
+_RETINA_METRICS = ('accuracy', 'negative_log_likelihood', 'ece',
+                   'retention_auroc_auc', 'retention_accuracy_auc')
+# Compute cost metrics.
+_COMPUTE_METRICS = ('exaflops', 'tpu_days', 'gflops', 'ms_step')
+
+# Pretraining datasets.
+_UPSTREAM_DATASETS = ('jft/entity:1.0.0', 'imagenet21k')
+# Fewshot datasets of interest.
+_FEWSHOT_DATASETS = ('imagenet', 'pets', 'birds', 'col_hist', 'cifar100',
+                     'caltech', 'cars', 'dtd', 'uc_merced')
+
+
+def default_selected_metrics() -> List[str]:
+  """Returns the list of metrics we care about for the big paper."""
+  metrics = list(_COMPUTE_METRICS)
+
+  # In-distribution metrics
+  metrics.extend(f'test_{m}' for m in _SPLIT_METRICS)
+
+  # Out of distribution metrics
+  metrics.extend(f'{dset}_{m}' for dset, m in itertools.product(
+      ['cifar_10h', 'imagenet_real'], _SPLIT_METRICS))
+
+  # Out of distribution detection metrics
+  metrics.extend(
+      f'ood_{dset}_msp_auroc'
+      for dset in ['cifar10', 'cifar100', 'svhn_cropped', 'places365_small']
+  )
+
+  # Fewshot metrics
+  metrics.extend(
+      f'z/{dset}_{k}shot'
+      for dset, k in itertools.product(_FEWSHOT_DATASETS, [1, 5, 10, 25])
+  )
+
+  # Retina metrics
+  metrics.extend(
+      f'{prefix}/{metric}' for metric, prefix in itertools.product(
+          _RETINA_METRICS, ['in_domain_test', 'ood_test'])
+  )
+
+  return metrics
 
 
 def _is_higher_better(metric: str) -> bool:
@@ -67,10 +114,16 @@ def get_unique_value(df: pd.DataFrame, col: str) -> Any:
   return unique_values[0]
 
 
+def row_wise_unique_non_nan(df: pd.DataFrame) -> pd.Series:
+  """Checks there is exactly one non-NA in each row, and returns its value."""
+  non_nan_counts = df.notna().sum(axis=1)
+  if not (non_nan_counts <= 1).all():
+    raise ValueError(f'Rows {df[non_nan_counts>=1]} have multiple set values!')
+  return df.fillna(axis=1, method='bfill')[df.columns[0]]
+
+
 def is_hyperparameter(
-    column: str,
-    auxiliary_hparams: Iterable[str] = ('learning_rate', _MODEL_COL)
-) -> bool:
+    column: str, auxiliary_hparams: Iterable[str] = ('learning_rate',)) -> bool:
   """Returns True if the column corresponds to a hyperparameter."""
   return column.startswith(_HPARAM_PREFIX) or column in auxiliary_hparams
 
@@ -159,13 +212,105 @@ def get_tuned_results(df: pd.DataFrame,
     verbose: if True, logs to stdout which hyperparameters were swept over, and
       which values are optimal.
 
-  Returns: A subset of `df` corresponding to the experimental runs with optimal
+  Returns:
+    A subset of `df` corresponding to the experimental runs with optimal
     hyperparameters.
   """
   df = df.copy()
   best_hps = get_best_hyperparameters(
-      df, tuning_metric, marginalization_hparams=marginalization_hparams,
+      df,
+      tuning_metric,
+      marginalization_hparams=marginalization_hparams,
       verbose=verbose)
   for k, v in best_hps.items():
     df = df[df[k] == v]
+  return df
+
+
+def _fill_upstream_test_metrics(df: pd.DataFrame) -> pd.DataFrame:
+  """Copies validation metrics to test metrics on upstream datasets.
+
+  Upstream datasets (Imagenet21K and JFT) have no dedicated test set, since they
+  are used to pretrain models rather than evaluate them. To simplify downstream
+  plotting analysis, we consider upstream validation metrics to be "test"
+  metrics since validation metrics are typically dropped.
+
+  Args:
+    df: pd.DataFrame where rows correspond to model classes, and columns to
+      reported metrics.
+
+  Returns:
+    A copy of `df` where the rows corresponding to models trained on upstream
+    datasets have their in-distribution test metrics filled with in-distribution
+    validation metrics.
+  """
+  df = df.copy()
+  for m in _SPLIT_METRICS:
+    if f'val_{m}' in df.columns:
+      idx = df[_DATASET_COL].isin(_UPSTREAM_DATASETS)
+      df.loc[idx, f'test_{m}'] = df.loc[idx, f'val_{m}']
+  return df
+
+
+def process_tuned_results(
+    df: pd.DataFrame,
+    relevant_metrics: Optional[Iterable[str]] = None,
+) -> pd.DataFrame:
+  """Cleans and reformats the dataframe of all results used for the big paper.
+
+  Args:
+    df: pd.DataFrame where each row represents the average performance of a
+      (model, train_dataset) pair with optimal hyperparameters across a variety
+      of metrics. This dataframe is expected to have columns `[_MODEL_COL,
+      _DATASET_COL]`, as well as all chosen metrics to analyze
+      (`relevant_metrics`).
+    relevant_metrics: Optional list of all metrics to analyze; defaults to
+      metrics produced by `default_selected_metrics()`. These metrics must all
+      be columns of `df`.
+
+  Returns:
+    pd.DataFrame where each row corresponds to a model in `df`, and each column
+    is a 2-level multiindex of stucture `(metric, dataset)`, where metrics
+    correspond to `relevant_metrics` and datasets are the available training
+    sets. Note that not all `(metric, dataset)` columns exist, since some
+    metrics are only reported on a subset of available training sets (for
+    example, we don't report Cifar100 OOD numbers when training on Cifar100).
+  """
+  df = _fill_upstream_test_metrics(df)
+
+  if relevant_metrics is None:
+    relevant_metrics = default_selected_metrics()
+
+  df = df.groupby([_MODEL_COL,
+                   _DATASET_COL])[relevant_metrics].mean().reset_index()
+
+  df = df.pivot(index=_MODEL_COL, columns=_DATASET_COL, values=relevant_metrics)
+  df = df.dropna(axis=1, how='all')
+  df.columns.set_names(['metric', 'dataset'], inplace=True)
+
+  # Fewshot metrics are only reported on upstream datasets. For each fewshot
+  # metric `m`, `df[m]` is a dataframe with two columns: one for each possible
+  # upstream dataset. Each row of `df[m]` only has one column with a non NaN
+  # value, so we collapse both columns into a single column.
+  fewshot_metrics = (c for c in df.columns.levels[0] if c.startswith('z/'))
+  for fewshot_metric in fewshot_metrics:
+    fewshot_match = re.match(r'z/(.*)_(\d*)shot', fewshot_metric)
+    if fewshot_match is not None:
+      dset, k = fewshot_match.groups()
+    else:
+      raise ValueError(f'Unconsistent fewshot metric {fewshot_metric}.')
+    df[f'{k}shot_prec@1',
+       f'few-shot {dset}'] = row_wise_unique_non_nan(df[fewshot_metric])
+
+    df.drop(columns=fewshot_metric, level=0, inplace=True)
+
+  # For now, we only care about compute metrics on upstream datasets, and only
+  # one of the two upstream compute columns has a non-NaN value for each model.
+  # We process the compute metrics similarly to the fewshot metrics.
+  compute_metrics = [c for c in df.columns.levels[0] if c in _COMPUTE_METRICS]
+  for metric in compute_metrics:
+    compute_vals = row_wise_unique_non_nan(df[metric][list(_UPSTREAM_DATASETS)])
+    df.drop(metric, axis=1, level=0, inplace=True)
+    df[metric, 'compute'] = compute_vals
+
   return df
--- experimental/big_paper/colab_utils_test.py
+++ experimental/big_paper/colab_utils_test.py
@@ -18,11 +18,12 @@
 from absl.testing import absltest
 from absl.testing import parameterized
 
+import numpy as np
 import pandas as pd
 import colab_utils  # local file import from experimental.big_paper
 
 
-def get_test_dataframe():
+def get_test_untuned_dataframe():
   # Experiment where we sweep `config.lr` over [.01, .02].
   # - config.lr = .01 has better validation loss
   # - config.lr = .02 has better AUROC.
@@ -60,6 +61,41 @@ def get_test_dataframe():
   df['model'] = 'Det'
   df['config.dataset'] = 'Cifar10'
   return df
+
+
+def get_test_tuned_dataset():
+  det_rows = [{
+      'model': 'Det',
+      'config.dataset': 'jft/entity:1.0.0',
+      'exaflops': 100,
+      'z/cars_5shot': .6,
+      'val_loss': .3,
+  }, {
+      'model': 'Det',
+      'config.dataset': 'jft/entity:1.0.0',
+      'exaflops': 150,
+      'z/cars_5shot': .8,
+      'val_loss': .5,
+  }, {
+      'model': 'Det',
+      'config.dataset': 'cifar10',
+      'cifar_10h_ece': .1,
+      'test_loss': .2,
+  }]
+
+  be_rows = [{
+      'model': 'BE',
+      'config.dataset': 'imagenet21k',
+      'exaflops': 200,
+      'z/cars_5shot': .9,
+      'val_loss': .3
+  }, {
+      'model': 'BE',
+      'config.dataset': 'cifar10',
+      'cifar_10h_ece': .02,
+      'test_loss': .01,
+  }]
+  return pd.DataFrame(det_rows + be_rows)
 
 
 class ColabUtilsTest(parameterized.TestCase):
@@ -114,6 +150,33 @@ class ColabUtilsTest(parameterized.TestCase):
                                 'Expected unique value in column'):
       colab_utils.get_unique_value(df, column)
 
+  @parameterized.named_parameters(
+      dict(
+          testcase_name='_no_nans',
+          df=pd.DataFrame({
+              'a': [2, 2, np.nan, np.nan],
+              'b': [np.nan, np.nan, 'x', np.nan],
+              'c': [np.nan, np.nan, np.nan, 'y']
+          }),
+          expected_series=pd.Series([2, 2, 'x', 'y'], name='a'),
+      ),
+      dict(
+          testcase_name='_with_nans',
+          df=pd.DataFrame({
+              'a': [2, np.nan, 3, np.nan],
+              'b': [np.nan, np.nan, np.nan, 'x']
+          }),
+          expected_series=pd.Series([2, np.nan, 3, 'x'], name='a')),
+  )
+  def test_row_wise_unique_non_nan(self, df, expected_series):
+    pd.testing.assert_series_equal(
+        colab_utils.row_wise_unique_non_nan(df), expected_series)
+
+  def test_row_wise_unique_non_nan_fails(self):
+    df = pd.DataFrame({'a': [1, 2, np.nan], 'b': [1, np.nan, 3]})
+    with self.assertRaisesRegex(ValueError, 'have multiple set values'):
+      colab_utils.row_wise_unique_non_nan(df)
+
   @parameterized.parameters(('config.seed', (), True),
                             ('learning_rate', (), False),
                             ('learning_rate', ('learning_rate', 'model'), True),
@@ -130,7 +193,7 @@ class ColabUtilsTest(parameterized.TestCase):
   )
   def test_get_sweeped_hyperparameters(self, marginalization_hparams,
                                        expected_sweeped_params):
-    df = get_test_dataframe()
+    df = get_test_untuned_dataframe()
     actual_sweeped_hparams = colab_utils.get_sweeped_hyperparameters(
         df, marginalization_hparams)
     self.assertSetEqual(
@@ -167,18 +230,75 @@ class ColabUtilsTest(parameterized.TestCase):
   )
   def test_get_best_hyperparameters(self, metric, marginalization_hparams,
                                     expected_hparams):
-    df = get_test_dataframe()
+    df = get_test_untuned_dataframe()
     actual_hparams = colab_utils.get_best_hyperparameters(
         df, metric, marginalization_hparams)
     self.assertDictEqual(actual_hparams, expected_hparams)
 
   @parameterized.parameters(('val_loss', .01), ('auroc', .02))
   def test_get_tuned_results(self, tuning_metric, best_lr):
-    df = get_test_dataframe()
+    df = get_test_untuned_dataframe()
     actual_results = colab_utils.get_tuned_results(
         df, tuning_metric=tuning_metric)
     expected_results = actual_results[actual_results['config.lr'] == best_lr]
     pd.testing.assert_frame_equal(actual_results, expected_results)
+
+  def test_fill_upstream_test_metrics(self):
+    input_df = pd.DataFrame({
+        'config.dataset': ['jft/entity:1.0.0', 'cifar10', 'imagenet21k'],
+        'val_loss': [.1, .2, .3],
+        'val_prec@1': [.9, .8, .4],
+        'val_ece': [.2, .5, .8],
+        'val_calib_auc': [.2, .5, .8],
+        'test_loss': [np.nan, .9, np.nan],
+        'test_prec@1': [.2, .1, np.nan]  # First value should be overwritten.
+    })
+    expected_df = pd.DataFrame({
+        'config.dataset': ['jft/entity:1.0.0', 'cifar10', 'imagenet21k'],
+        'val_loss': [.1, .2, .3],
+        'val_prec@1': [.9, .8, .4],
+        'val_ece': [.2, .5, .8],
+        'val_calib_auc': [.2, .5, .8],
+        'test_loss': [.1, .9, .3],
+        'test_prec@1': [.9, .1, .4],
+        'test_ece': [.2, np.nan, .8],
+        'test_calib_auc': [.2, np.nan, .8],
+    })
+    pd.testing.assert_frame_equal(
+        colab_utils._fill_upstream_test_metrics(input_df), expected_df)
+
+  def test_processed_tuned_results(self):
+    relevant_metrics = [
+        'test_loss', 'cifar_10h_ece', 'z/cars_5shot', 'exaflops'
+    ]
+    input_df = get_test_tuned_dataset()
+    expected_be_results = {
+        'model': 'BE',
+        ('test_loss', 'imagenet21k'): .3,  # Filled to val_loss.
+        ('test_loss', 'jft/entity:1.0.0'): np.nan,
+        ('test_loss', 'cifar10'): .01,
+        ('5shot_prec@1', 'few-shot cars'): .9,
+        ('cifar_10h_ece', 'cifar10'): .02,
+        ('exaflops', 'compute'): 200,
+    }
+
+    expected_det_results = {
+        'model': 'Det',
+        ('test_loss', 'jft/entity:1.0.0'): (.3 + .5) / 2,  # Filled to val_loss.
+        ('test_loss', 'imagenet21k'): np.nan,
+        ('test_loss', 'cifar10'): .2,
+        ('cifar_10h_ece', 'cifar10'): .1,
+        ('5shot_prec@1', 'few-shot cars'): (.6 + .8) / 2,
+        ('exaflops', 'compute'): (100 + 150) / 2,
+    }
+
+    expected_df = pd.DataFrame([expected_be_results,
+                                expected_det_results]).set_index('model')
+    expected_df.columns = pd.MultiIndex.from_tuples(
+        expected_df.columns, names=['metric', 'dataset'])
+    result_df = colab_utils.process_tuned_results(input_df, relevant_metrics)
+    pd.testing.assert_frame_equal(result_df.sort_index(axis=1),
+                                  expected_df.sort_index(axis=1))
 
 
 if __name__ == '__main__':

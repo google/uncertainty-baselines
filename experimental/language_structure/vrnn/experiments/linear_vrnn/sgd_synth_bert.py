0e78a3165ad6d98bf48fdb516b3d07a0fec9e2cf
Quan Yuan <yquan@google.com>
2022-04-19 21:49:51 -0700

    Check in experiment configs for DSTCSynth GloVe & BERT
    PiperOrigin-RevId: 442993401

--- experimental/language_structure/vrnn/experiments/linear_vrnn/sgd_synth_bert.py
+++ experimental/language_structure/vrnn/experiments/linear_vrnn/sgd_synth_bert.py
@@ -13,16 +13,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-r"""Vizier for linear VRNN for SGDSynthDataset.
+r"""Vizier for linear VRNN for MultiWoZSynthDataset.
 
 """
 
+import model_config  # local file import from experimental.language_structure.vrnn
 import sgd_synth_tmpl as tmpl  # local file import from experimental.language_structure.vrnn.experiments.linear_vrnn
 
 
 def get_config():
   """Returns the configuration for this experiment."""
-  config = tmpl.get_config()
+  config = tmpl.get_config(
+      encoder_embedding_type=model_config.BERT_EMBED,
+      shared_embedding=False,
+      bert_embedding_type='base')
+
+  config.train_batch_size = 4
+  config.eval_batch_size = 4
 
   return config
 
@@ -34,9 +41,10 @@ def get_sweep(hyper):
                   hyper.discrete([0.25 * i for i in range(5)])),
       hyper.sweep('config.psl_constraint_learning_weight',
                   hyper.discrete([0., 0.001, 0.005, 0.01, 0.05, 0.1])),
-      hyper.sweep('config.bow_loss_weight',
-                  hyper.discrete([0.1, 0.5, 1, 2, 5])),
-      hyper.sweep('config.base_learning_rate', hyper.discrete([5e-4, 1e-3]))
+      hyper.sweep('config.bow_loss_weight', hyper.discrete([0.1, 0.5, 1, 2,
+                                                            5])),
+      hyper.sweep('config.base_learning_rate',
+                  hyper.discrete([3e-5, 5e-5, 1e-4, 3e-4]))
   ]
   sweep = hyper.product(domain)
   return sweep

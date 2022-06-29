#psl Make PSL logic and loss function configurable and fix normalization
PiperOrigin-RevId: 457958791

experimental/language_structure/psl/psl_model.py
@@ -25,17 +25,21 @@ _LINEAR_LOSS = 'linear'
 _LOG_LOSS = 'log'
 
 
-def normalize(val, should_normalize: bool = True):
-  if should_normalize:
-    val_sum = tf.maximum(tf.reduce_sum(val), _EPSILON)
-    return val / val_sum
-  return val
+def normalize(val, should_normalize: bool = False):
+  if not should_normalize:
+    return val
+  val_sum = tf.maximum(tf.reduce_sum(val), _EPSILON)
+  return val / val_sum
 
 
 class PSLModel(abc.ABC):
   """Abstract class for PSL constraints."""
 
-  def __init__(self, rule_weights: List[float], rule_names: List[str],
+  def __init__(self,
+               rule_weights: List[float],
+               rule_names: List[str],
+               logic: str = 'lukasiewicz',
+               loss_function: str = _LINEAR_LOSS,
                **kwargs) -> None:
     assert len(rule_weights) == len(
         rule_names), 'Rule weights and rule names must be the same length.'
@@ -43,6 +47,8 @@ class PSLModel(abc.ABC):
     self.rule_functions = [getattr(self, rule_name) for rule_name in rule_names]
     self.rule_weights = rule_weights
     self.kwargs = kwargs
+    self.logic = logic
+    self.loss_function = loss_function
 
   def generate_predicates(self, data: tf.Tensor):
     """Generates potentials used throughout the rules."""
@@ -64,17 +70,14 @@ class PSLModel(abc.ABC):
                         loss_function: str = _LINEAR_LOSS) -> float:
     """Calculates loss for a soft rule."""
 
-    if logic == 'lukasiewicz':
-      if loss_function == _LINEAR_LOSS:
-        body, head = PSLModel.soft_imply(body, head, logic=logic)
-        # body -> head = max(1 - SUM(1 - body_i) - head, 0)
-        return tf.reduce_sum(tf.nn.relu(1. - tf.add_n(body) - head))
-      else:
-        truth_value = PSLModel.soft_imply2(body, head, logic=logic)
-        prob = tf.clip_by_value(truth_value, _EPSILON, 1. - _EPSILON)
-        return tf.reduce_mean(-tf.reduce_sum(tf.math.log(prob), axis=-1))
+    if loss_function == _LINEAR_LOSS:
+      body, head = PSLModel.soft_imply(body, head, logic=logic)
+      # body -> head = max(1 - SUM(1 - body_i) - head, 0)
+      return tf.reduce_sum(tf.nn.relu(1. - tf.add_n(body) - head))
     else:
-      raise ValueError('Unsuported logic: %s' % (logic,))
+      truth_value = PSLModel.soft_imply2(body, head, logic=logic)
+      prob = tf.clip_by_value(truth_value, _EPSILON, 1. - _EPSILON)
+      return tf.reduce_mean(-tf.reduce_sum(tf.math.log(prob), axis=-1))
 
   # Keep it as a part of the implementation of the original loss function.
   @staticmethod
@@ -92,54 +95,45 @@ class PSLModel(abc.ABC):
   @staticmethod
   def soft_imply2(body: List[tf.Tensor],
                   head: tf.Tensor,
-                  logic: str = 'lukasiewicz',
-                  should_normalize: bool = False) -> tf.Tensor:
+                  logic: str = 'lukasiewicz') -> tf.Tensor:
     """Soft logical implication."""
     if logic == 'lukasiewicz':
       body = PSLModel.soft_and(body)
       # A -> B = min(1, 1 - A + B)
-      return normalize(tf.minimum(1., 1 - body + head), should_normalize)
+      return tf.minimum(1., 1 - body + head)
     else:
       raise ValueError('Unsuported logic: %s' % (logic,))
 
   @staticmethod
-  def soft_and(body: List[tf.Tensor],
-               logic: str = 'lukasiewicz',
-               should_normalize: bool = False) -> tf.Tensor:
+  def soft_and(body: List[tf.Tensor], logic: str = 'lukasiewicz') -> tf.Tensor:
     """Soft logical implication."""
-    num_bodies = len(body)
-    if num_bodies == 0:
-      return tf.constant(0)
     if logic == 'lukasiewicz':
+      num_bodies = len(body)
+      if num_bodies == 0:
+        return tf.constant(0)
       # A & B = max(0, A + B - 1)
       val = body[0]
       for i in range(1, num_bodies):
-        val = tf.nn.relu(val + body[i] - 1)
-      return normalize(val, should_normalize)
+        val = normalize(tf.nn.relu(val + body[i] - 1))
+      return val
     else:
       raise ValueError('Unsuported logic: %s' % (logic,))
 
   @staticmethod
-  def soft_not(predicate: tf.Tensor,
-               logic: str = 'lukasiewicz',
-               should_normalize: bool = False) -> tf.Tensor:
+  def soft_not(predicate: tf.Tensor, logic: str = 'lukasiewicz') -> tf.Tensor:
     """Soft logical negation."""
     if logic == 'lukasiewicz':
       # !predicate = 1 - predicate
-      return normalize(1. - predicate, should_normalize)
+      return normalize(1. - predicate)
     else:
       raise ValueError('Unsuported logic: %s' % (logic,))
 
-  @staticmethod
-  def template_rx_implies_sx(r_x: tf.Tensor,
-                             s_x: tf.Tensor,
-                             logic: str = 'lukasiewicz') -> float:
+  def template_rx_implies_sx(self, r_x: tf.Tensor, s_x: tf.Tensor) -> float:
     """Template for R(x) -> S(x).
 
     Args:
       r_x: a (batch_size, example_size) tensor.
       s_x: a (batch_size, example_size) tensor.
-      logic: the type of logic being used.
 
     Returns:
       A computed loss for this type of rule.
@@ -147,20 +141,16 @@ class PSLModel(abc.ABC):
     body = [r_x]
     head = s_x
 
-    return PSLModel.compute_rule_loss(body, head, logic=logic)
+    return PSLModel.compute_rule_loss(body, head, logic=self.logic)
 
-  @staticmethod
-  def template_rx_and_sx_implies_tx(r_x: tf.Tensor,
-                                    s_x: tf.Tensor,
-                                    t_x: tf.Tensor,
-                                    logic: str = 'lukasiewicz') -> float:
+  def template_rx_and_sx_implies_tx(self, r_x: tf.Tensor, s_x: tf.Tensor,
+                                    t_x: tf.Tensor) -> float:
     """Template for R(x) & S(x) -> T(x).
 
     Args:
       r_x: a (batch_size, example_size) tensor.
       s_x: a (batch_size, example_size) tensor.
       t_x: a (batch_size, example_size) tensor.
-      logic: the type of logic being used.
 
     Returns:
       A computed loss for this type of rule.
@@ -168,13 +158,10 @@ class PSLModel(abc.ABC):
     body = [r_x, s_x]
     head = t_x
 
-    return PSLModel.compute_rule_loss(body, head, logic=logic)
+    return PSLModel.compute_rule_loss(body, head, logic=self.logic)
 
-  @staticmethod
-  def template_rxy_and_sy_implies_tx(r_xy: tf.Tensor,
-                                     s_y: tf.Tensor,
-                                     t_x: tf.Tensor,
-                                     logic: str = 'lukasiewicz') -> float:
+  def template_rxy_and_sy_implies_tx(self, r_xy: tf.Tensor, s_y: tf.Tensor,
+                                     t_x: tf.Tensor) -> float:
     """Template for R(x,y) & S(y) -> T(x).
 
     Converts s_y and t_x into (batch_size, example_size, example_size) tensors,
@@ -184,7 +171,6 @@ class PSLModel(abc.ABC):
       r_xy: a (batch_size, example_size, example_size) tensor.
       s_y: a (batch_size, example_size) tensor.
       t_x: a (batch_size, example_size) tensor.
-      logic: the type of logic being used.
 
     Returns:
       A computed loss for this type of rule.
@@ -195,15 +181,11 @@ class PSLModel(abc.ABC):
     body = [r_xy, s_y_matrix]
     head = t_x_matrix
 
-    return PSLModel.compute_rule_loss(body, head, logic=logic)
+    return PSLModel.compute_rule_loss(body, head, logic=self.logic)
 
-  @staticmethod
-  def template_rxy_and_sy_and_tx_implies_ux(
-      r_xy: tf.Tensor,
-      s_y: tf.Tensor,
-      t_x: tf.Tensor,
-      u_x: tf.Tensor,
-      logic: str = 'lukasiewicz') -> float:
+  def template_rxy_and_sy_and_tx_implies_ux(self, r_xy: tf.Tensor,
+                                            s_y: tf.Tensor, t_x: tf.Tensor,
+                                            u_x: tf.Tensor) -> float:
     """Template for R(x,y) & S(y) & T(x) -> U(x).
 
     Converts s_y, t_x, and u_x into (batch_size, example_size, example_size)
@@ -214,7 +196,6 @@ class PSLModel(abc.ABC):
       s_y: a (batch_size, example_size) tensor.
       t_x: a (batch_size, example_size) tensor.
       u_x: a (batch_size, example_size) tensor.
-      logic: the type of logic being used.
 
     Returns:
       A computed loss for this type of rule.
@@ -226,16 +207,13 @@ class PSLModel(abc.ABC):
     body = [r_xy, s_y_matrix, t_x_matrix]
     head = u_x_matrix
 
-    return PSLModel.compute_rule_loss(body, head, logic=logic)
+    return PSLModel.compute_rule_loss(body, head, logic=self.logic)
 
-  @staticmethod
-  def template_rxy_and_sy_and_tx_and_ux_implies_vx(
-      r_xy: tf.Tensor,
-      s_y: tf.Tensor,
-      t_x: tf.Tensor,
-      u_x: tf.Tensor,
-      v_x: tf.Tensor,
-      logic: str = 'lukasiewicz') -> float:
+  def template_rxy_and_sy_and_tx_and_ux_implies_vx(self, r_xy: tf.Tensor,
+                                                   s_y: tf.Tensor,
+                                                   t_x: tf.Tensor,
+                                                   u_x: tf.Tensor,
+                                                   v_x: tf.Tensor) -> float:
     """Template for R(x,y) & S(y) & T(x) & U(x) -> V(x).
 
     Converts s_y, t_x, u_x, and v_x into (batch_size, example_size,
@@ -248,7 +226,6 @@ class PSLModel(abc.ABC):
       t_x: a (batch_size, example_size) tensor.
       u_x: a (batch_size, example_size) tensor.
       v_x: a (batch_size, example_size) tensor.
-      logic: the type of logic being used.
 
     Returns:
       A computed loss for this type of rule.
@@ -261,7 +238,7 @@ class PSLModel(abc.ABC):
     body = [r_xy, s_y_matrix, t_x_matrix, u_x_matrix]
     head = v_x_matrix
 
-    return PSLModel.compute_rule_loss(body, head, logic=logic)
+    return PSLModel.compute_rule_loss(body, head, logic=self.logic)
 
   @staticmethod
   def _unary_to_binary(predicate: tf.Tensor, transpose: bool) -> tf.Tensor:
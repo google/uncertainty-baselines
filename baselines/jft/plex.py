b6217e704d0ac546e950e8b99f9d0842a84e7d8b
Dustin Tran <trandustin@google.com>
2022-07-13 18:52:35 -0700

    Rename HetBE for ViT to be Plex as canonical script.
    PiperOrigin-RevId: 460854074

--- baselines/jft/plex.py
+++ baselines/jft/plex.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""BatchEnsemble Vision Transformer."""
+"""ViT-Plex."""
 
 import functools
 import multiprocessing

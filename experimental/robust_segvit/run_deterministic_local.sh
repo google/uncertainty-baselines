ff87a4ea10afb6d5246e88e3dbbd739f8ea0bf99
Kelly Buchanan <ekellbuchanan@gmail.com>
2022-11-12 18:01:49 -0500

    add bash script to train model with runlocal

---
+++ experimental/robust_segvit/run_deterministic_local.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# train toy model using wandb
+#wandb sweep run_toy_mac.yaml
+# before make sure we can run code vanilla version:
+
+DATASET='ade20k_ind'  # or cityscapes
+DATASET='street_hazards'
+DATASET='cityscapes'
+
+base_output_dir="gs://ub-ekb/segmenter/${DATASET}/deterministic"
+
+# Debug on Mac OS X platform
+use_gpu=False
+if [ "$(uname)" = "Darwin" ] ; then
+tpu=False
+num_cores=1
+batch_size=5
+elif [ "$(uname)" = "Linux" ]; then
+tpu='local'
+num_cores=8
+batch_size=8
+fi
+
+config_file="configs/${DATASET}/deterministic.py:runlocal"
+run_name="local"
+output_dir="${base_output_dir}/${run_name}"
+python deterministic.py \
+--output_dir=${output_dir} \
+--num_cores=$num_cores \
+--use_gpu=$use_gpu \
+--config=${config_file} \
+--config.batch_size=${batch_size} \
+--tpu=${tpu} \

d6c8cd6abb8d7a3f18ebb4548ce0a2bfc4b76694
Kelly Buchanan <ekellbuchanan@gmail.com>
2022-11-12 22:39:16 -0500

    remove call to computescoreaucmetric

--- experimental/robust_segvit/custom_segmentation_trainer.py
+++ experimental/robust_segvit/custom_segmentation_trainer.py
@@ -54,7 +54,7 @@ import os
 import resource
 import sys
 import robustness_metrics as rm
-from metrics_multihost import ComputeOODAUCMetric, ComputeScoreAUCMetric
+from metrics_multihost import ComputeOODAUCMetric
 from metrics_multihost import host_all_gather_metrics
 
 Batch = Dict[str, jnp.ndarray]

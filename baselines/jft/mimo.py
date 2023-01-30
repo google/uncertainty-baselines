Fix Imagenet ReaL masking bug.
PiperOrigin-RevId: 505670998

baselines/jft/mimo.py
@@ -340,7 +340,7 @@ def main(config, output_dir):
   @partial(jax.pmap, axis_name='batch')
   def evaluation_fn(params, images, labels, mask):
     # Ignore the entries with all zero labels for evaluation.
-    mask *= labels.max(axis=1)
+    mask *= (labels.max(axis=1) > 0).astype(labels.dtype)
     tiled_images = jnp.tile(images, (1, 1, 1, config.model.ensemble_size,))
     tiled_logits, out = model.apply({'params': flax.core.freeze(params)},
                                     tiled_images,